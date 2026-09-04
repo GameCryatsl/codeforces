89cc2880eb2f06947b129b5bae0fc2228e27f020
GameCryatsl <117077704+GameCryatsl@users.noreply.github.com>
2026-09-04 17:13:48 +0900

    Implement main function to check sum of inputs

---
+++ 69A.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, x = 0, y = 0, z = 0;
+    
+    cin >> n;
+    
+    for(int i = 0; i < n; i++) {
+        int a, b, c;
+        
+        cin >> a >> b >> c;
+        
+        x+=a;
+        y+=b;
+        z+=c;
+    }
+    
+    if(!(x || y || z)) {
+        cout << "YES";
+    }
+    else cout << "NO";
+}
