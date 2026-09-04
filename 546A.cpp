80362e64f526aa2f6959505096aadec54f900676
GameCryatsl <117077704+GameCryatsl@users.noreply.github.com>
2026-09-04 17:09:27 +0900

    Create 546A.cpp

---
+++ 546A.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int k, n, w;
+    
+    cin >> k >> n >> w;
+    
+    cout << max(0, w * (w + 1) / 2 * k - n);
+}
